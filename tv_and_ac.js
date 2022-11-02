0d6998e0f60e1055961a94411dd33a8108bf35c1
sanvisawhney <96869508+sanvisawhney@users.noreply.github.com>
2022-11-02 15:53:56 +0530

    Add files via upload

---
+++ tv_and_ac.js
@@ -0,0 +1,31 @@
+status = "";
+
+function preload(){
+    img = loadImage("tvandac.webp");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    objectDetector =  ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+
+function modelLoaded(){
+    status = "true";
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+}
+
+function draw(){
+    image(img, 0, 0 , 600, 500);
+}
